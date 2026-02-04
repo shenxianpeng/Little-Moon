e144ea6e9c0782dcf9c0b7636faab647c49ae7d4
shenxianpeng <xianpeng.shen@gmail.com>
2026-02-04 20:59:44 +0200

    Initial Commit

---
+++ Little Moon/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Little Moon
+//
+//  Created by Xianpeng Shen on 2026/2/4.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Little Moon/Little_MoonApp.swift
@@ -0,0 +1,17 @@
+//
+//  Little_MoonApp.swift
+//  Little Moon
+//
+//  Created by Xianpeng Shen on 2026/2/4.
+//
+
+import SwiftUI
+
+@main
+struct Little_MoonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
